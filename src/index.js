374a6eb8c1244375e46d5eec6c824cb924305673
kovaaalex <kovalenko.alex04@gmail.com>
2024-08-30 01:22:31 +0300

    fix: change cases

--- src/index.js
+++ src/index.js
@@ -1,43 +1,41 @@
 module.exports = function toReadable(number) {
-    const numbers = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
-    const ten = ["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
-    const tenfoldNumbers = ["twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
-    
-    if (number === 0) return "zero";
-    
-    let result = "";
-    let copyNumber = number;
-    let numberLength = number.toString().length;
+    const numbers = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+    const tenNumbers = ["ten", "eleven", "twelve", "thirteen", "fourteen" , "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"]
+    const tenFoldNumbers = ["twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
 
-    while (numberLength) {
-        let currentDigit = Math.floor(copyNumber / Math.pow(10, numberLength - 1));
-        copyNumber = copyNumber % Math.pow(10, numberLength - 1);
-        switch (numberLength % 3) {
+    let result = "", copyNumber = number, numberLength = number.toString().length
+
+    if(!number) return "zero"
+    while(numberLength){
+        let currentDigit = Math.floor(copyNumber / Math.pow(10, numberLength - 1))
+        copyNumber = copyNumber % Math.pow(10, numberLength - 1)
+        switch (numberLength % 3){
             case 0: {
-                result += numbers[currentDigit] + " hundred";
-                if (copyNumber > 0) result += " ";
-                break;
-            }
-            case 2: {
-                if (!currentDigit) break
-                if (currentDigit === 1) {
-                    result += ten[copyNumber];
-                    numberLength--;
-                    copyNumber = 0;
-                } else {
-                    result += tenfoldNumbers[currentDigit - 2];
-                    if (copyNumber % Math.pow(10, numberLength - 1) > 0) result += " ";
-                }
-                break;
+                result += numbers[currentDigit] + " hundred"
+                if (copyNumber) result += " "
+                break
             }
             case 1: {
-                result += numbers[currentDigit];
-                if (numberLength > 1) result += " ";
-                break;
+                result += numbers[currentDigit]
+                if (copyNumber) result += " "
+                break
+            }
+            case 2:
+            {
+                if(!currentDigit) break
+                if(currentDigit === 1){
+                    result += tenNumbers[copyNumber]
+                    copyNumber = 0
+                    numberLength--
+                }
+                else {
+                    result += tenFoldNumbers[currentDigit - 2]
+                    if (copyNumber % Math.pow(10, numberLength - 1) > 0) result += " "
+                }
+                break
             }
         }
-        numberLength--;
+        numberLength--
     }
-    
-    return result.trim();
+    return result.trim()
 }
